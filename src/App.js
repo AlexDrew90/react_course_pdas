55f6b6549c26ab6063b56dc09e4fd75b58d5c8f0
Alex Drew <alexander.william.drew@gmail.com>
2023-08-17 21:31:22 +0800

    Added images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import ProfileCard from './ProfileCard'
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 let App = () => {
+
   return(
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard title='Alexa' handle='@alex99'/>
-      <ProfileCard title='Cortana' handle='@Cortana32'/>
-      <ProfileCard title='Siri' handle='@siri199'/>
+
+      <ProfileCard title='Alexa' handle='@alex99' image={AlexaImage} />
+      <ProfileCard title='Cortana' handle='@Cortana32' image={CortanaImage} />
+      <ProfileCard title='Siri' handle='@siri199' image={SiriImage} />
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,7 +1,8 @@
-let ProfileCard = ({title, handle}) => {
+let ProfileCard = ({title, handle, image}) => {
   // const title = props.title
   // const handle = props.handle
   return <div>
+          <div><img src={image} alt="pda logo"/></div>
           <p>The assistant is called {title} and the handle is {handle}.</p>
         </div>
 }
